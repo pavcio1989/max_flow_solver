3ebfc5d077606b4d52af5077dd3458955b08e7e0
pawel <pavcio1989@gmail.com>
2025-03-17 14:23:49 +0100

    prepare base solver class

--- src/entities/edges.py
+++ src/entities/edges.py
@@ -1,6 +1,22 @@
 class Edge:
-    def __init__(self):
-        pass
+    flow: float = 0
+    residual = None
+
+    def __init__(self, start, end, capacity):
+        self.start = start
+        self.end = end
+        self.capacity = capacity
+
+    def is_residual(self):
+        return self.capacity == 0
+
+    def get_remaining_capacity(self):
+        return self.capacity - self.flow
+
+    def augment(self, bottleneck):
+        self.flow += bottleneck
+        self.residual.flow -= bottleneck
 
     def __str__(self):
-        print(f"Edge")
+        print(f"Edge {self.start} -> {self.end} | flow = {self.flow} \
+        | capacity = {self.capacity} | is residual = {self.is_residual()}")
--- src/solvers/base.py
+++ src/solvers/base.py
@@ -1,14 +1,50 @@
+from abc import abstractmethod
+from collections import defaultdict
+
+from ..entities.edges import Edge
+
+
+INFINITY = 1000000
+
+
 class NetworkFlowSolverBase:
+    solved = False
+    visited_token = 1
+    visited = []
+    __max_flow = None
+    __graph = defaultdict(list)
+
     def __init__(self, n, s, t):
         self.n = n
         self.s = s
         self.t = t
 
-    def add_edge(self, start_node, end_node, capacity):
+    @abstractmethod
+    def solve(self):
         pass
 
-    def get_max_flow(self):
-        pass
+    def add_edge(self, start_node, end_node, capacity):
+        if capacity <= 0:
+            raise Exception("Forward edge capacity <=0")
+        edge1 = Edge(start_node, end_node, capacity)
+        edge2 = Edge(end_node, start_node, 0)
+
+        edge1.residual = edge2
+        edge2.residual = edge1
+
+        self.__graph[start_node].append(edge1)
+        self.__graph[end_node].append(edge2)
 
     def get_graph(self):
-        pass
+        self.execute()
+        return self.__graph
+
+    def get_max_flow(self):
+        self.execute()
+        return self.__max_flow
+
+    def execute(self):
+        if self.solved:
+            return
+        self.solved = True
+        self.solve()
